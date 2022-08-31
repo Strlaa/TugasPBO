725902c685982624c08bff8f8d6ef93b179887ab
Strlaa <sapahayoo66@gmail.com>
2022-08-31 12:56:01 +0700

    first commit

---
+++ luas persegi panjang.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	float p, l, luas;
+	
+	cout<<"LUAS PERSEGI PANJANG"<<endl;
+	cout<<"--------------------"<<endl;
+	cout<<"Masukkan panjang: ";
+	cin>>p;
+	cout<<"Masukkan lebar: ";
+	cin>>l;
+	luas = p*l;
+	cout<<"Luas nya adalah "<<luas<<endl;
+	
+
+	return 0;
+}
